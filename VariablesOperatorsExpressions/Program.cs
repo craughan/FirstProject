c7231bd1b5039d82b4a391df5a4f39ed0c7c4111
Cameron Maughan <craughan01@gmail.com>
2020-08-13 16:36:08 -0700

    Example code for VOE section

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -6,7 +6,12 @@ namespace VariablesOperatorsExpressions
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World! Zwei");
+			int a = 15;
+			int b = 2;
+			double c = 2.5;
+			bool trust = false;
+			Console.WriteLine((a + c) - (b * c) + a);	// prints 27.5
+			Console.WriteLine((!(a < b) || trust) && (!((c >= b) || trust)));	// prints False
 			Console.ReadLine();
 		}
 	}
