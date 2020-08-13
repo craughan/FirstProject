c16c783a74bbbdf3a6539fbb110f7f6269102502
Cameron Maughan <craughan01@gmail.com>
2020-08-13 08:50:13 -0700

    Add first files

---
+++ FirstConsoleProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstConsoleProgram
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
+// 1. Create a Git repository
+//    - .gitignore
+// 2. Add files to the Git repository
+// 3. Commit those files to the Git repository
+// 4. Add a remote
+// 5. Push to the remote
