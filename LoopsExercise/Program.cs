63b6a5fb586a349873ea9562ff988b191125ef90
Cameron Maughan <craughan01@gmail.com>
2020-08-13 18:07:39 -0700

    'git commit -am' doesn't add new files!

---
+++ LoopsExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LoopsExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int score = 0;
+
+			// print number of times loops runs, 10X
+			int i = 0;
+			while (i < 10)
+			{
+				i++;
+				Console.WriteLine("This while loop has run " + i + " time(s).");
+			}
+
+			// same thing, but for loop
+			for (int j = 1; j <= 10; j++)
+			{
+				Console.WriteLine($"This for loop has run {j} time(s).");
+				
+				// when score reaches 100, break loop
+				if (score < 100)
+				{
+					score += 20;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
