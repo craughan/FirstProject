4d81418abfbeae54f00305c0782a9252d4642fd2
Cameron Maughan <craughan01@gmail.com>
2020-08-13 17:12:46 -0700

    Add braces to if statements

--- IfStatementsExercise/Program.cs
+++ IfStatementsExercise/Program.cs
@@ -12,21 +12,31 @@ namespace IfStatementsExercise
 
 			// let the player know if they are dead or alive
 			if (playerAlive)
+			{
 				Console.WriteLine("You are still alive.");
+			}
 			else
+			{
 				Console.WriteLine("You are dead.");
+			}
 
 			// let the player know when they are vulnerable when the timer runs out
 			if (invulnerabilityTimer > 0)
 			{
 				if (playerAlive)
+				{
 					invulnerabilityTimer--;
+				}
 				Console.WriteLine("You are invulnerable.");
 			}
 			else if (invulnerabilityTimer == 0)
+			{
 				Console.WriteLine("You are vulnerable.");
+			}
 			else if (invulnerabilityTimer < 0)
+			{
 				invulnerabilityTimer = 0;
+			}
 
 		}
 	}
