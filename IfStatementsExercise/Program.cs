4c5ab9df3465c7284450025610fb42e93d78af81
Cameron Maughan <craughan01@gmail.com>
2020-08-13 17:03:07 -0700

    IfStatementExercise complete

---
+++ IfStatementsExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IfStatementsExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool playerAlive = true;
+			int invulnerabilityTimer = 3;
+			invulnerabilityTimer -= 1;
+
+			// let the player know if they are dead or alive
+			if (playerAlive)
+				Console.WriteLine("You are still alive.");
+			else
+				Console.WriteLine("You are dead.");
+
+			// let the player know when they are vulnerable when the timer runs out
+			if (invulnerabilityTimer > 0)
+			{
+				if (playerAlive)
+					invulnerabilityTimer--;
+				Console.WriteLine("You are invulnerable.");
+			}
+			else if (invulnerabilityTimer == 0)
+				Console.WriteLine("You are vulnerable.");
+			else if (invulnerabilityTimer < 0)
+				invulnerabilityTimer = 0;
+
+		}
+	}
+}
